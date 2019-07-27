38fdef9452d36d4f47cd9fd3123e460aee6b6e0b
Hikalight <scarlankhikari@gmail.com>
2019-07-27 10:39:55 +0800

    Site updated: 2019-07-27 10:39:50

---
+++ categories/test.java
@@ -0,0 +1,13 @@
+public class test{
+
+    public static void main(String [] args){
+
+        System.out.println("刘威");
+        System.out.println("41152620000721631X");
+        System.out.println("18");
+        System.out.println("河南工业大学");
+        System.out.println("计算机科学与技术");
+        System.out.println("2018");
+        System.out.println("4年");
+    }
+}
